76850c0f4acc3754d2532f48af93530e7190a493
Sandeep Kumar <satyasandeep786@gmail.com>
2022-12-03 17:22:35 +0530

    feat: Add Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { ToastContainer } from "react-toastify";
+import React, { useEffect, useContext } from "react";
+import Routing from "./Routing";
+import Loader from "./layouts/Loader";
+import { GlobalContext } from "./context/context";
+import { ethers } from "ethers";
+import Config from "./Config";
+// import { makeStyles } from "@material-ui/core/styles";
+// import { ToastContainer } from "react-toastify";
 // import Navbar from "./components/Navbar";
 // import TabsBody from "./components/TabsBody";
 // import { useSmartAccountContext } from "./contexts/SmartAccountContext";
@@ -8,12 +13,10 @@ import { ToastContainer } from "react-toastify";
 // import Button from "./components/Button";
 
 const App: React.FC = () => {
+  const { loading, addWeb3ProviderToContext, setLoading } =
+  useContext(GlobalContext);
 
-
- 
-
-  return <p>Hello World</p>;
+  return <div>{loading ? <Loader /> : <Routing />}</div>;
 };
-
 
 export default App;
