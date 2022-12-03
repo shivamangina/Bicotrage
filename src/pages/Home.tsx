83e0add07386145a967b62c4b97daa03b6ed0815
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-12-03 20:23:57 +0530

    fix: Fix home UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import Card from './Card'
-import Stepper from './Stepper'
+import Card from "./Card";
+import Stepper from "./Stepper";
 
 function Home() {
   return (
-    <div>
-      <button
-        className="flex content-center w-full px-6 py-1 mb-3 text-base leading-6 text-white bg-purple-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto ">
-        Add Step
-      </button>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 m-6">
-        <Stepper />
-        <Card />
-      </div>
-    </div>
-  )
+    <>
+      <section className="px-2 py-20 bg-white md:px-0">
+        <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+          <div className="flex flex-col items-center mt-12 text-center tails-selected-element">
+            <span className="relative inline-flex w-full md:w-auto">
+              <a
+                href="#_"
+                type="button"
+                className="inline-flex items-center justify-center w-full px-8 py-3 text-base font-bold leading-6 text-white bg-purple-600 border border-transparent rounded-md md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+              >
+                Add Step
+              </a>
+            </span>
+            <br/>
+          </div>
+          <div className="flex flex-wrap items-center sm:-mx-3">
+            <div className="container items-center">
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 m-6">
+                <Stepper />
+                <Card />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
