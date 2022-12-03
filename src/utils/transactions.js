6fa263759cacdb8e487f9722bfecc69b8927875d
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-12-03 20:02:47 +0530

    feat: Add batch transaction code

---
+++ src/utils/transactions.js
@@ -0,0 +1,34 @@
+const batchTransaction = async (txnArray, wallet) => {
+  try {
+    console.log("Batching transactions");
+
+    const feeQuotes = await wallet.prepareRefundTransactionBatch({
+      transactions: txnArray,
+    });
+
+    // making transaction with version, set feeQuotes[1].tokenGasPrice = 6
+    const transaction = await wallet.createRefundTransactionBatch({
+      transactions: txnArray,
+      feeQuote: feeQuotes[1],
+    });
+
+    console.log("transaction", transaction);
+
+    let gasLimit = {
+      hex: "0x1E8480",
+      type: "hex",
+    };
+    // send transaction internally calls signTransaction and sends it to connected relayer
+    const txHash = await wallet.sendTransaction({
+      tx: transaction,
+      gasLimit,
+    });
+    
+    console.log(txHash);
+    return txHash;
+
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
